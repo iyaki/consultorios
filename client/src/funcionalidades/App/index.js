11fe6400977d4ca80321637bdf32b9012386c1e3
iyaki <ivanyaki96@gmail.com>
2021-07-10 02:05:16 +0000

    Layout basico

--- client/src/funcionalidades/App/index.js
+++ client/src/funcionalidades/App/index.js
@@ -1,9 +1,20 @@
-import { Route, Switch } from 'react-router-dom'
+import { AppBar, Button, IconButton, Toolbar } from '@material-ui/core'
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded'
+import { Link, Route, Switch } from 'react-router-dom'
 import './styles.css'
 
 function App () {
   return (
     <div>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton className='Toolbar-text-color' component={Link} to='/'>
+            <HomeRoundedIcon />
+          </IconButton>
+          <Button className='Toolbar-text-color' component={Link} to='/agendas'>Agendas</Button>
+          <Button className='Toolbar-text-color' component={Link} to='/facturacion'>Facturación</Button>
+        </Toolbar>
+      </AppBar>
       <Switch>
         <Route exact path='/'>
           /
