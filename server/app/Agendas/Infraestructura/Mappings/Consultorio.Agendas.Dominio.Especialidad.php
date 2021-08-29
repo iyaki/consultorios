20a8c3d9495883614dfce3ac45a9784ea73cb32b
iyaki <ivanyaki96@gmail.com>
2021-08-29 22:56:28 +0000

    Cambio tipos hardcodeados en entidad por constantes

--- server/app/Agendas/Infraestructura/Mappings/Consultorio.Agendas.Dominio.Especialidad.php
+++ server/app/Agendas/Infraestructura/Mappings/Consultorio.Agendas.Dominio.Especialidad.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace Consultorio\Agendas\Infraestructura\Mappings;
 
+use Doctrine\DBAL\Types\Types;
+
 /**
  * @psalm-suppress UndefinedGlobalVariable
  */
 $builder = new \Doctrine\ORM\Mapping\Builder\ClassMetadataBuilder($metadata);
 $builder->setTable('agendas_especialidades');
 $builder
-    ->createField('id', 'guid')
+    ->createField('id', Types::GUID)
     ->makePrimaryKey()
     ->build()
 ;
-$builder->addField('nombre', 'string');
+$builder->addField('nombre', Types::STRING);
