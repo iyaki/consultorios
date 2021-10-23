3b8a8ef27505b7c3dfde9779161ea3dfbe21d53f
iyaki <ivanyaki96@gmail.com>
2021-10-23 13:39:41 -0300

    Preparativos para continuous integration

--- WebApp/backend/public/dev-server.php
+++ WebApp/backend/public/dev-server.php
@@ -2,6 +2,6 @@
 
 declare(strict_types=1);
 
-require_once '/root/.asdf/installs/php/8.0.11/.composer/vendor/autoload.php';
+require_once '/root/php/.composer/vendor/autoload.php';
 
 require __DIR__ . '/index.php';
