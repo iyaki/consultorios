db8a56bdef941b8b8cd2097cac1c92854d051cfd
iyaki <ivanyaki96@gmail.com>
2022-03-05 19:29:03 -0300

    Ajuste en la configuracion de rector y ecs para WebApp

--- WebApp/backend/ecs.php
+++ WebApp/backend/ecs.php
@@ -2,26 +2,8 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+use function Consultorios\DevToolsSettings\getECSConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/app',
-    ]);
-    $parameters->set(Option::SKIP, [
-        __DIR__ . '/ecs.php',
-        __DIR__ . '/rector.php',
-    ]);
-    $parameters->set(Option::CACHE_DIRECTORY, \sys_get_temp_dir() . '/.ecs_cache');
-    $parameters->set(Option::LINE_ENDING, "\n");
-    $parameters->set(Option::SKIP, [
-        PhpCsFixer\Fixer\PhpUnit\PhpUnitStrictFixer::class,
-    ]);
+require __DIR__ . '/vendor/autoload.php';
 
-    $containerConfigurator->import(SetList::PSR_12);
-    $containerConfigurator->import(SetList::CLEAN_CODE);
-    $containerConfigurator->import(SetList::COMMON);
-};
+return getECSConfigurator([__DIR__ . '/app/']);
