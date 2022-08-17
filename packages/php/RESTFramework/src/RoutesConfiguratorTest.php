525c85b0168e494d2bec42118b5f9def62db9e05
iyaki <ivanyaki96@gmail.com>
2022-08-17 19:28:42 +0000

    Tests unitarios para RoutesConfigurator

---
+++ packages/php/RESTFramework/src/RoutesConfiguratorTest.php
@@ -0,0 +1,335 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Consultorios\RESTFramework;
+
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\Response\TextResponse;
+use Laminas\Diactoros\ServerRequest;
+use Laminas\HttpHandlerRunner\RequestHandlerRunnerInterface;
+use Laminas\Stratigility\MiddlewarePipe;
+use Laminas\Stratigility\MiddlewarePipeInterface;
+use Mezzio\Application;
+use Mezzio\MiddlewareContainer;
+use Mezzio\MiddlewareFactory;
+use Mezzio\Router\RouteCollector;
+use Mezzio\Router\RouterInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class RoutesConfiguratorTest extends TestCase
+{
+
+    public function testBasePath(): void
+    {
+        $r = new RoutesConfigurator(
+            $this->createMock(Application::class),
+            $this->createMock(ResponseFactoryInterface::class)
+        );
+
+        $this->assertSame('', $r->basePath());
+
+        $newBasePath = 'asd/';
+
+        $r = $r->withBasePath($newBasePath);
+
+        $this->assertSame($newBasePath, $r->basePath());
+    }
+
+    public function testResponseFactory(): void
+    {
+        $r = new RoutesConfigurator(
+            $this->createMock(Application::class),
+            $this->createMock(ResponseFactoryInterface::class)
+        );
+
+        $this->assertInstanceOf(ResponseFactory::class, $r->responseFactory([]));
+    }
+
+    public function testGet(): void
+    {
+        $app = $this->application();
+
+        $rouesConfigurator = $this->routesConfigurator($app);
+
+        $path = 'holu';
+        $rouesConfigurator->get($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($path, $route->getPath());
+        $this->assertCount(1, $route->getAllowedMethods());
+        $this->assertContains('GET', $route->getAllowedMethods());
+    }
+
+    public function testGetWithBasePath(): void
+    {
+        $app = $this->application();
+        $basePath = '/wiiii';
+
+        $rouesConfigurator = $this
+            ->routesConfigurator($app)
+            ->withBasePath($basePath)
+        ;
+
+        $path = '/holu';
+        $rouesConfigurator->get($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($basePath . $path, $route->getPath());
+        $this->assertCount(1, $route->getAllowedMethods());
+        $this->assertContains('GET', $route->getAllowedMethods());
+    }
+
+    public function testPost(): void
+    {
+        $app = $this->application();
+
+        $rouesConfigurator = $this->routesConfigurator($app);
+
+        $path = 'holu';
+        $rouesConfigurator->post($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($path, $route->getPath());
+        $this->assertCount(1, $route->getAllowedMethods());
+        $this->assertContains('POST', $route->getAllowedMethods());
+    }
+
+    public function testPostWithBasePath(): void
+    {
+        $app = $this->application();
+        $basePath = '/wiiii';
+
+        $rouesConfigurator = $this
+            ->routesConfigurator($app)
+            ->withBasePath($basePath)
+        ;
+
+        $path = '/holu';
+        $rouesConfigurator->post($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($basePath . $path, $route->getPath());
+        $this->assertCount(1, $route->getAllowedMethods());
+        $this->assertContains('POST', $route->getAllowedMethods());
+    }
+
+    public function testPut(): void
+    {
+        $app = $this->application();
+
+        $rouesConfigurator = $this->routesConfigurator($app);
+
+        $path = 'holu';
+        $rouesConfigurator->put($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($path, $route->getPath());
+        $this->assertCount(1, $route->getAllowedMethods());
+        $this->assertContains('PUT', $route->getAllowedMethods());
+    }
+
+    public function testPutWithBasePath(): void
+    {
+        $app = $this->application();
+        $basePath = '/wiiii';
+
+        $rouesConfigurator = $this
+            ->routesConfigurator($app)
+            ->withBasePath($basePath)
+        ;
+
+        $path = '/holu';
+        $rouesConfigurator->put($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($basePath . $path, $route->getPath());
+        $this->assertCount(1, $route->getAllowedMethods());
+        $this->assertContains('PUT', $route->getAllowedMethods());
+    }
+
+    public function testPatch(): void
+    {
+        $app = $this->application();
+
+        $rouesConfigurator = $this->routesConfigurator($app);
+
+        $path = 'holu';
+        $rouesConfigurator->patch($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($path, $route->getPath());
+        $this->assertCount(1, $route->getAllowedMethods());
+        $this->assertContains('PATCH', $route->getAllowedMethods());
+    }
+
+    public function testPatchWithBasePath(): void
+    {
+        $app = $this->application();
+        $basePath = '/wiiii';
+
+        $rouesConfigurator = $this
+            ->routesConfigurator($app)
+            ->withBasePath($basePath)
+        ;
+
+        $path = '/holu';
+        $rouesConfigurator->patch($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($basePath . $path, $route->getPath());
+        $this->assertCount(1, $route->getAllowedMethods());
+        $this->assertContains('PATCH', $route->getAllowedMethods());
+    }
+
+    public function testDelete(): void
+    {
+        $app = $this->application();
+
+        $rouesConfigurator = $this->routesConfigurator($app);
+
+        $path = 'holu';
+        $rouesConfigurator->delete($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($path, $route->getPath());
+        $this->assertCount(1, $route->getAllowedMethods());
+        $this->assertContains('DELETE', $route->getAllowedMethods());
+    }
+
+    public function testDeleteWithBasePath(): void
+    {
+        $app = $this->application();
+        $basePath = '/wiiii';
+
+        $rouesConfigurator = $this
+            ->routesConfigurator($app)
+            ->withBasePath($basePath)
+        ;
+
+        $path = '/holu';
+        $rouesConfigurator->delete($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($basePath . $path, $route->getPath());
+        $this->assertCount(1, $route->getAllowedMethods());
+        $this->assertContains('DELETE', $route->getAllowedMethods());
+    }
+
+    public function testAny(): void
+    {
+        $app = $this->application();
+
+        $rouesConfigurator = $this->routesConfigurator($app);
+
+        $path = 'holu';
+        $rouesConfigurator->any($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($path, $route->getPath());
+        $this->assertSame(null, $route->getAllowedMethods());
+    }
+
+    public function testAnyWithBasePath(): void
+    {
+        $app = $this->application();
+        $basePath = '/wiiii';
+
+        $rouesConfigurator = $this
+            ->routesConfigurator($app)
+            ->withBasePath($basePath)
+        ;
+
+        $path = '/holu';
+        $rouesConfigurator->any($path, fn() => null);
+
+        $routes = $app->getRoutes();
+        $route = reset($routes);
+
+        $this->assertSame($basePath . $path, $route->getPath());
+        $this->assertSame(null, $route->getAllowedMethods());
+    }
+
+    public function testPipe(): void
+    {
+        $middlewarePipe = new MiddlewarePipe();
+        $app = $this->application($middlewarePipe);
+
+        $rouesConfigurator = $this->routesConfigurator($app);
+
+        $bodyMessage = 'Middleware de prueba :)';
+
+        $rouesConfigurator->pipe(fn() => new class($bodyMessage) implements MiddlewareInterface {
+            private readonly string $bodyMessage;
+
+            public function __construct(string $bodyMessage)
+            {
+                $this->bodyMessage = $bodyMessage;
+            }
+
+            public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+            {
+                return new TextResponse($this->bodyMessage);
+            }
+        });
+
+        $response = $middlewarePipe->handle(new ServerRequest());
+
+        $this->assertSame($bodyMessage, (string) $response->getBody());
+    }
+
+    public function __clone()
+    {
+        throw new \Exception('Cloning this class is not allowed');
+    }
+
+    public function __sleep()
+    {
+        throw new \Exception('This class can\'t be serialized');
+    }
+
+    private function routesConfigurator(Application $app): RoutesConfigurator
+    {
+        return new RoutesConfigurator(
+            $app,
+            $this->createMock(ResponseFactoryInterface::class)
+        );
+    }
+
+    private function application(?MiddlewarePipeInterface $middlewarePipe = null): Application
+    {
+        return new Application(
+            new MiddlewareFactory($this->createMock(MiddlewareContainer::class)),
+            $middlewarePipe ?? new MiddlewarePipe(),
+            new RouteCollector($this->createMock(RouterInterface::class)),
+            $this->createMock(RequestHandlerRunnerInterface::class),
+        );
+    }
+}
